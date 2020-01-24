4197a55cc3e7e10039d0f938ab50276c991ea773
yazseyit77 <yazgeldi.seyidov@gmail.com>
2020-01-24 10:05:37 -0800

    listed items design

--- risto-client/src/components/Restaurants.js
+++ risto-client/src/components/Restaurants.js
@@ -1,20 +1,45 @@
 import React, { Component } from "react";
+import { Button, Icon, Image, Item, Label, Menu } from "semantic-ui-react";
 
 class Restaurants extends Component {
   render() {
     console.log(this.props.restaurants);
     const restaurants = this.props.restaurants.map(rest => (
-      <li key={rest.restaurant.id}>
-        <a href={rest.restaurant.url}>
-          <h2>{rest.restaurant.name}</h2>
-        </a>
-        <h5>Address: {rest.restaurant.location.address}</h5>
-        <h4>Open hours: {rest.restaurant.timings}</h4>
-        <a href={rest.restaurant.menu_url}>
-          <h4>Menu</h4>
-        </a>
-        <img src={rest.restaurant.thumb} alt="no img" />
-      </li>
+      <Item.Group divided>
+        <Item>
+          <Item.Image
+            avatar
+            src={
+              rest.restaurant.thumb
+                ? rest.restaurant.thumb
+                : "https://react.semantic-ui.com/images/wireframe/image.png"
+            }
+          />
+          <Item.Content>
+            <br />
+            <Item.Header as="a">
+              <a href={rest.restaurant.url}>{rest.restaurant.name}</a>
+            </Item.Header>
+            <Item.Meta>
+              <span className="cinema">
+                Open hours: {rest.restaurant.timings}
+              </span>
+            </Item.Meta>
+            <Item.Description>
+              {rest.restaurant.location.address}
+            </Item.Description>
+            <Item.Extra>
+              <a href={rest.restaurant.menu_url}>Menu</a>
+              <Icon
+                name="heart outline"
+                color="red"
+                size="big"
+                floated="right"
+              />
+            </Item.Extra>
+          </Item.Content>
+        </Item>
+      </Item.Group>
     ));
     return (
       <div>
